0588c3e94bb39391bf123dbcbaec5e6b9596b712
acidMyke <13977709+acidMyke@users.noreply.github.com>
2023-11-27 19:48:36 +0800

    basic log using iconify

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Icon from '@iconify/svelte';
+
+  export let scale = 1;
+  $: bgImageWidthHeight = 72 * scale;
+</script>
+
+<div class="grid grid-cols-1 grid-rows-1 rounded-xl bg-base-300 p-2">
+  <Icon
+    icon="mdi:run"
+    class="col-start-1 row-start-1 mx-auto block w-full stroke-primary text-primary"
+    width={bgImageWidthHeight}
+    height={bgImageWidthHeight}
+  />
+  <p class="col-start-1 row-start-1 text-center font-mono text-2xl font-black text-base-content opacity-50">
+    Track 2.4
+  </p>
+</div>
