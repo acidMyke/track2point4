29d0fdaa4894480b583fed08c498597c593a1d6a
acidMyke <13977709+acidMyke@users.noreply.github.com>
2023-11-27 20:11:11 +0800

    added routes

---
+++ src/routes/(with-navbar)/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Logo from '$lib/components/Logo.svelte';
+</script>
+
+<div class="navbar">
+  <div class="navbar-start">
+    <Logo scale={0.5} />
+  </div>
+</div>
+
+<main>
+  <slot />
+</main>
---
+++ src/routes/(with-navbar)/result/[index]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+  import { readSaved, conducts } from '$lib/stores/data';
+</script>
